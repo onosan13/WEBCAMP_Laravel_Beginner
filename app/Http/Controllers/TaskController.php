8e5580f4472310a921ef5932200b81715744c36f
shogo onoda <shogo@example.com>
2024-05-23 13:55:33 +0000

    タスクの登録処理まで書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\TaskRegisterPostRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task as TaskModel;
 
 class TaskController extends Controller
 {
@@ -24,6 +26,26 @@ class TaskController extends Controller
     {
         //validate済のデータの取得
         $datum = $request->validated();
-        var_dump($datum); exit;
+        //
+        // $id = Auth::id();
+        //var_dump($datum,$id); exit;
+
+        //user_idの追加
+        $datum['user_id']=Auth::id();
+
+        //テーブルへのINSERT
+        try{
+            $r=TaskModel::create($datum);
+        }catch(\Throwable $e){
+            //本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
+
+        //タスク登録成功
+        $request->session()->flash('front.task_register_success', true);
+
+        //
+        return redirect('/task/list');
     }
 }
--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -5,15 +5,25 @@
 
 {{--メインコンテンツ--}}
 @section('contets')
-        <h1>タスクの登録(未実装)</h1>
+        <h1>タスクの登録</h1>
+            @if (session('front.task_register_success') == true)
+                タスクを登録しました！！<br>
+            @endif
+            @if($errors->any())
+                <div>
+                    @foreach($errors->all() as $error)
+                        {{ $error }}<br>
+                    @endforeach
+                </div>
+            @endif
             <form action="/task/register" method="post">
                 @csrf
-                タスク名:<input name="name"><br>
-                期限:<input name="period" type="date"><br>
-                タスク詳細:<textarea name="detail"></textarea><br>
-                重要度:<label><input type="radio" name="priority" value="1">低い</label> /
-                    <label><input type="radio" name="priority" value="2" checked>普通</label> /
-                    <label><input type="radio" name="priority" value="3">高い</label><br>
+                タスク名:<input name="name" value="{{old('name')}}"><br>
+                期限:<input name="period" type="date" value="{{(old('period'))}}"><br>
+                タスク詳細:<textarea name="detail">{{old('detail')}}</textarea><br>
+                重要度:<label><input type="radio" name="priority" value="1" @if(old('priority')==1) checked @endif>低い</label> /
+                    <label><input type="radio" name="priority" value="2" @if(old('priority',2)==2) checked @endif>普通</label> /
+                    <label><input type="radio" name="priority" value="3" @if(old('priority')==3) checked @endif>高い</label><br>
                 <button>タスクを登録する</button>
             </form>
 
