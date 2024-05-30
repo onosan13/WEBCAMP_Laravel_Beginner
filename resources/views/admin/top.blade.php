9cec657c0f8e99e61844c048c94a96357cc93739
shogo onoda <shogo@example.com>
2024-05-30 07:17:28 +0000

    管理画面のユーザ一覧機能を書いた

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -2,15 +2,6 @@
 
 {{-- メインコンテンツ --}}
 @section('contets')
-        <menu label="リンク">
-            <a href="./user_list.html">ユーザー一覧</a><br>
-            管理画面機能1<br>
-            管理画面機能2<br>
-            管理画面機能3<br>
-            管理画面機能4<br>
-            <a href="/admin/logout">ログアウトする</a><br>
-        </menu>
-
         <h1>管理画面</h1>
         (アクセス傾向のグラフや警告などを表示する事が多い)<br>
 @endsection
